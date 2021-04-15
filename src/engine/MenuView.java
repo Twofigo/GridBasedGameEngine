1d916e49bafb6fdc5b8ddb1ba0d8366c1ffa6995
August Baaz <august.baaz@gmail.com>
2021-04-15 18:22:08 +0200

    fixed buttons layout better ish fuck you

--- src/engine/MenuView.java
+++ src/engine/MenuView.java
@@ -11,24 +11,23 @@ public class MenuView extends View{
         super(options, actions);
 
         buttonPanel = new JPanel();
-        buttonPanel.setLayout(new GridLayout(buttons.length,1,10,10));
+        buttonPanel.setLayout(new GridLayout(buttons.length,1,0,10));
         for(int k=0;k<buttons.length; k++){
             buttonPanel.add(buttons[k]);
         }
 
         buttonPanel.setBackground(Color.green);
-        buttonPanel.setSize(200,800);
         panel.setSize(200,400);
         panel.add(buttonPanel, BorderLayout.CENTER);
     }
 
     @Override
     protected void setSize(int width, int height) {
-        panel.setSize(width,height);
-
+        /*
         for(JButton b : buttons){
-            b.setPreferredSize(new Dimension(width/3,((height)/buttons.length)-10));
-        }
-        buttonPanel.setSize(width/3, height);
+            b.setPreferredSize(new Dimension(width/3,40));
+        }*/
+        panel.setSize(width,height);
+        buttonPanel.setPreferredSize(new Dimension(width/3, height-20));
     }
 }
