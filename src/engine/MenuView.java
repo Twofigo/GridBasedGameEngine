47e8b5dee9d9ad7fce0f6b889f82fd5e1280bbb5
August Baaz <august.baaz@gmail.com>
2021-04-15 18:12:06 +0200

    fucked with buttons

--- src/engine/MenuView.java
+++ src/engine/MenuView.java
@@ -11,19 +11,24 @@ public class MenuView extends View{
         super(options, actions);
 
         buttonPanel = new JPanel();
-        buttonPanel.setLayout(new GridLayout(buttons.length,1));
+        buttonPanel.setLayout(new GridLayout(buttons.length,1,10,10));
         for(int k=0;k<buttons.length; k++){
             buttonPanel.add(buttons[k]);
         }
 
         buttonPanel.setBackground(Color.green);
-        setSize();
-        panel.add(buttonPanel);
+        buttonPanel.setSize(200,800);
+        panel.setSize(200,400);
+        panel.add(buttonPanel, BorderLayout.CENTER);
     }
 
     @Override
     protected void setSize(int width, int height) {
-        buttonPanel.setSize(width/3, height);
         panel.setSize(width,height);
+
+        for(JButton b : buttons){
+            b.setPreferredSize(new Dimension(width/3,((height)/buttons.length)-10));
+        }
+        buttonPanel.setSize(width/3, height);
     }
 }
