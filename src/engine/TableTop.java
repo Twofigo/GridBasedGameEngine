b7edc10dfa0a5139531736f2d5cfa946b11ab732
August Baaz <august.baaz@gmail.com>
2021-04-17 14:26:21 +0200

    finnished up tableTop, allowing multiple layers of tiles

--- src/engine/TableTop.java
+++ src/engine/TableTop.java
@@ -5,10 +5,18 @@ public class TableTop {
     private final int width;
     private final int height;
 
-    public TableTop(Board[] boards, int width, int height) {
+    public TableTop(Board[] boards) throws Exception {
         this.boards = boards;
-        this.width = width;
-        this.height = height;
+        this.width = boards[0].width();
+        this.height = boards[0].height();
+        for (Board b: boards) {
+            if(width!=b.width() || height!=b.height()) throw new Exception("board size mismatch");
+        }
+    }
+    public TableTop(Board board) {
+        this.boards = new Board[]{board};
+        this.width = boards[0].width();
+        this.height = boards[0].height();
     }
     public int width() {
         return width;
