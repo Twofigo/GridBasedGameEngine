1fbf50335548df7857a60f686d4a2a1fa5ce4300
August Baaz <august.baaz@gmail.com>
2021-04-20 18:45:59 +0200

    added a level and improved sokoban

--- src/games/TestGame/Sokoban/Sokoban.java
+++ src/games/TestGame/Sokoban/Sokoban.java
@@ -36,45 +36,20 @@ public class Sokoban extends PuppetMaster {
         th.addTexture("dc-dngn/wall/stone2_gray0.png", "box");
         th.addTexture("dc-dngn/wall/crystal_wall02.png", "ActiveBox");
         th.addTexture("dc-dngn/floor/tutorial_pad.png", "plate");
+        th.addTexture("dc-dngn/wall/stone_brick1.png", "wall");
         //System.setProperty("sun.java2d.opengl", "true");
 
         // level setup
+        levels = new ArrayList<Level>();
+        createLevel1();
+        createLevel2();
+        createLevel3();
+
         p = new Player("Elf");
         victoryProgress = 0;
+        setTableTop(levels.get(0));
+        levels.get(0).getForeground().place(p,0,0);
 
-        levels = new ArrayList<Level>();
-        Level l = new Level(8,8, 4);
-
-        Box b1 = new Box("box", "ActiveBox");
-        Box b2 = new Box("box", "ActiveBox");
-        Box b3 = new Box("box","ActiveBox");
-        Box b4 = new Box("box","ActiveBox");
-        l.getForeground().place(b1,2,2);
-        l.getForeground().place(b2,3,2);
-        l.getForeground().place(b3,4,2);
-        l.getForeground().place(b4,5,2);
-        Plate plate = new Plate("plate");
-        l.getMiddleground().place(plate,0,7);
-        l.getMiddleground().place(plate,2,7);
-        l.getMiddleground().place(plate,5,7);
-        l.getMiddleground().place(plate,7,7);
-        l.getBackground().clear(new Tile("Floor1"));
-
-        levels.add(l);
-        setTableTop(l);
-        l.getForeground().place(p,0,0);
-
-        l = new Level(8,8, 999);
-        b1 = new Box("box", "ActiveBox");
-        b2 = new Box("box", "ActiveBox");
-        b3 = new Box("box","ActiveBox");
-        b4 = new Box("box","ActiveBox");
-        l.getForeground().place(b1,3,3);
-        l.getForeground().place(b2,4,3);
-        l.getForeground().place(b3,3,4);
-        l.getForeground().place(b4,2,4);
-        l.getBackground().clear(new Tile("Floor2"));
-        levels.add(l);
 
         // window setup
         bv = new BoardView("mainBoard", levels.get(0));
@@ -88,15 +63,148 @@ public class Sokoban extends PuppetMaster {
         win.updateSize(800, 600+40);
         win.draw();
     }
+    public void createLevel1(){
+        Level l = new Level(8,8, 4);
+        l.setPlayerSpawn(0,0);
+
+        Board bg = l.getBackground();
+        Board mg = l.getMiddleground();
+        Board fg = l.getForeground();
+
+        Box b1 = new Box("box", "ActiveBox");
+        Box b2 = new Box("box", "ActiveBox");
+        Box b3 = new Box("box","ActiveBox");
+        Box b4 = new Box("box","ActiveBox");
+        fg.place(b1,2,2);
+        fg.place(b2,3,2);
+        fg.place(b3,4,2);
+        fg.place(b4,5,2);
+
+        Plate plate = new Plate("plate");
+        mg.place(plate,0,7);
+        mg.place(plate,2,7);
+        mg.place(plate,5,7);
+        mg.place(plate,7,7);
+        bg.clear(new Tile("Floor1"));
+        levels.add(l);
+    }
+    public void createLevel2(){
+        Level l = new Level(11,11, 8);
+        l.setPlayerSpawn(5,5);
+
+        Board bg = l.getBackground();
+        Board mg = l.getMiddleground();
+        Board fg = l.getForeground();
+
+        Box b1 = new Box("box", "ActiveBox");
+        Box b2 = new Box("box", "ActiveBox");
+        Box b3 = new Box("box","ActiveBox");
+        Box b4 = new Box("box","ActiveBox");
+        fg.place(b1,2,2);
+        fg.place(b2,8,2);
+        fg.place(b3,2,8);
+        fg.place(b4,8,8);
+        Box b5 = new Box("box", "ActiveBox");
+        Box b6 = new Box("box", "ActiveBox");
+        Box b7 = new Box("box","ActiveBox");
+        Box b8 = new Box("box","ActiveBox");
+        fg.place(b5,5,4);
+        fg.place(b6,4,5);
+        fg.place(b7,6,5);
+        fg.place(b8,5,6);
+
+        Wall wall = new Wall("wall");
+        fg.place(wall,1,1);
+        fg.place(wall,2,1);
+        fg.place(wall,3,1);
+        fg.place(wall,4,1);
+        fg.place(wall,6,1);
+        fg.place(wall,7,1);
+        fg.place(wall,9,1);
+
+        fg.place(wall,9,2);
+
+        fg.place(wall,1,3);
+        fg.place(wall,3,3);
+        fg.place(wall,4,3);
+        fg.place(wall,6,3);
+        fg.place(wall,7,3);
+        fg.place(wall,9,3);
+
+        fg.place(wall,1,4);
+        fg.place(wall,3,4);
+        fg.place(wall,7,4);
+        fg.place(wall,9,4);
+
+        fg.place(wall,1,6);
+        fg.place(wall,3,6);
+        fg.place(wall,7,6);
+        fg.place(wall,9,6);
+
+        fg.place(wall,1,7);
+        fg.place(wall,3,7);
+        fg.place(wall,4,7);
+        fg.place(wall,6,7);
+        fg.place(wall,7,7);
+        fg.place(wall,9,7);
+
+        fg.place(wall,1,8);
+
+        fg.place(wall,1,9);
+        fg.place(wall,3,9);
+        fg.place(wall,4,9);
+        fg.place(wall,6,9);
+        fg.place(wall,7,9);
+        fg.place(wall,8,9);
+        fg.place(wall,9,9);
+
+        Plate plate = new Plate("plate");
+        mg.place(plate,5,0);
+        mg.place(plate,0,5);
+        mg.place(plate,10,5);
+        mg.place(plate,5,10);
+
+        mg.place(plate,5,2);
+        mg.place(plate,2,5);
+        mg.place(plate,8,5);
+        mg.place(plate,5,8);
+        bg.clear(new Tile("Floor2"));
+        levels.add(l);
+    }
+    public void createLevel3(){
+        Level l = new Level(8,8, 4);
+        l = new Level(8,8, 999);
+        l.setPlayerSpawn(0,0);
+
+        Board bg = l.getBackground();
+        Board mg = l.getMiddleground();
+        Board fg = l.getForeground();
+
+        Box b1 = new Box("box", "ActiveBox");
+        Box b2 = new Box("box", "ActiveBox");
+        Box b3 = new Box("box","ActiveBox");
+        Box b4 = new Box("box","ActiveBox");
+        fg.place(b1,3,3);
+        fg.place(b2,4,3);
+        fg.place(b3,3,4);
+        fg.place(b4,2,4);
+        l.getBackground().clear(new Tile("Floor2"));
+        levels.add(l);
+    }
     public void changeLevel(){
         if (currentLevelIndex+1>=levels.size()) return;
-        ((Level)getTableTop()).getForeground().pickup(p);
+        Level l;
+
+        l = ((Level)getTableTop());
+        l.getForeground().pickup(p);
         currentLevelIndex++;
         setTableTop(levels.get(currentLevelIndex));
         bv.setTableTop(getTableTop());
-        ((Level)getTableTop()).getForeground().place(p,0,0);
-    }
+        l = ((Level)getTableTop());
+        l.getForeground().place(p,l.getPlayerSpawnX(),l.getPlayerSpawnY());
+        bv.setOffset(l.width()*0.5, l.height()*0.5);
 
+    }
     public void updateVictoryProgress(int i){
         this.victoryProgress+=i;
     }
@@ -160,6 +268,34 @@ public class Sokoban extends PuppetMaster {
 
     }
 }
+class Level extends BasicTableTop{
+    private int playerSpawnX;
+    private int playerSpawnY;
+    private int victoryCondition;
+
+    public Level(int width, int height, int victoryCondition) {
+        super(width, height);
+        this.victoryCondition = victoryCondition;
+    }
+
+    public int getPlayerSpawnX() {
+        return playerSpawnX;
+    }
+
+    public void setPlayerSpawn(int x, int y) {
+        this.playerSpawnX = x;
+        this.playerSpawnY = y;
+    }
+
+    public int getPlayerSpawnY() {
+        return playerSpawnY;
+    }
+
+    public int getVictoryCondition() {
+        return victoryCondition;
+    }
+}
+
 interface MoveInto{
     public boolean moveInto(Sokoban p, Entity e);
 }
@@ -175,18 +311,6 @@ class Player extends Entity{
     @Override
     public void update() {
 
-    }
-}
-class Level extends BasicTableTop{
-    private int victoryCondition;
-
-    public Level(int width, int height, int victoryCondition) {
-        super(width, height);
-        this.victoryCondition = victoryCondition;
-    }
-
-    public int getVictoryCondition() {
-        return victoryCondition;
     }
 }
 class Box extends Entity implements MoveInto{
@@ -243,6 +367,18 @@ class Plate extends Tile implements MoveInto{
     @Override
     public boolean moveInto(Sokoban p, Entity e) {
         return true;
+    }
+}
+class Wall extends Tile implements MoveInto{
+    public Wall(Image texture) {
+        super(texture);
+    }
+    public Wall(String name) {
+        super(name);
+    }
+    @Override
+    public boolean moveInto(Sokoban p, Entity e) {
+        return false;
     }
 }
 class MoveTo extends Interaction{
